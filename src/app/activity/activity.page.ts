daf2812ddaf329288fc159e1f28f0109b7899dbb
GeraldSalazar <geraldsalazar4@gmail.com>
2020-11-17 13:46:41 -0600

    activity page

--- src/app/activity/activity.page.ts
+++ src/app/activity/activity.page.ts
@@ -3,6 +3,7 @@ import { Chronometer } from 'ngx-chronometer';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
 //import * as xml2js from 'xml2js';
 import { parseString } from 'xml2js';
+import { utf8Encode } from '@angular/compiler/src/util';
 
 enum StatusChonometer {
   desactived = 0,
@@ -33,7 +34,9 @@ export class ActivityPage implements OnInit {
   map: any = null;
   file: File;
   ActivityCoordinates: any = [];
-  uploadplaceholder : string = "new value";
+  userpath: any = null;
+
+
 
   constructor(private geolocation: Geolocation) { }
 
@@ -100,25 +103,25 @@ export class ActivityPage implements OnInit {
   loadGpxFromDevice(event) {
     this.file = event.target.files[0];
     let fileReader = new FileReader();
+    this.ActivityCoordinates = [];
     fileReader.onload = (e) => {
       parseString(fileReader.result, { explicitArray: true }, (error, result) => {
         if (error) {
           throw new Error(error);
         } else {
           const data = result.gpx.trk[0].trkseg[0].trkpt;
+          
+          
           data.forEach(element => {
-            //console.log(element.$.lat);
-            //const myLatlng = new google.maps.LatLng(parseFloat(element.$[0]),
-            //parseFloat(element.$[1]));
-
             const coords = {
               lat: +element.$.lat,
               lng: +element.$.lon
             }
-            //console.log(coords);
-            //console.log(typeof(myLatlng));
+
             this.ActivityCoordinates.push(coords);
           });
+          
+          
           this.setPathInMap();
         }
       });
@@ -126,14 +129,28 @@ export class ActivityPage implements OnInit {
     fileReader.readAsText(this.file);
   }
 
+
   setPathInMap(){
-    const userpath = new google.maps.Polyline({
+    this.userpath = new google.maps.Polyline({
       path: this.ActivityCoordinates,
       geodesic: true,
       strokeColor: "#FF0000",
       strokeOpacity: 1.0,
       strokeWeight: 2,
     }); 
-    userpath.setMap(this.map);
+    
+    this.map.setCenter(new google.maps.LatLng(this.ActivityCoordinates[0].lat, this.ActivityCoordinates[0].lng));
+    
+    this.userpath.setMap(this.map);  
   }
+
+
+  deletePathInMap(){
+    if(this.userpath === null){
+      console.log("No se ha cargado un archivo pgx")
+    }else{
+      this.userpath.setMap(null);
+    }
+  }
+
 }
